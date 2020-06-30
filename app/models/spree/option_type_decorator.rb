d37d0d9a26c9fc3020402f3de85d0deb60bf02a9
Ameer Batarseh <a.batarseh@floranow.com>
2020-06-30 12:37:06 +0300

    Remove unwanted translations

--- app/models/spree/option_type_decorator.rb
+++
@@ -1,9 +0,0 @@
-module Spree::OptionTypeDecorator
-  def self.prepended(base)
-    base.translates :name, :presentation, fallbacks_for_empty_translations: true
-  end
-  
-  Spree::OptionType.include SpreeGlobalize::Translatable
-end
-
-::Spree::OptionType.prepend(Spree::OptionTypeDecorator)
--- app/models/spree/promotion_decorator.rb
+++
@@ -1,9 +0,0 @@
-module Spree::PromotionDecorator
-  def self.prepended(base)
-    base.translates :name, :description, fallbacks_for_empty_translations: true
-  end
-
-  Spree::Promotion.include SpreeGlobalize::Translatable
-end
-
-::Spree::Promotion.prepend(Spree::PromotionDecorator)
